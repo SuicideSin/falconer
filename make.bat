950c4cba21f56bb739a9d1cb0b238f7b54187fa3
Mike Moss <mrmoss@alaska.edu>
2013-06-29 17:32:01 -0800

    Fixed OPT in make.bat to OPTS...still compiled but missing some statically linked libs.

--- make.bat
+++ make.bat
@@ -3,7 +3,7 @@
 set MSL=src/msl/2d.cpp src/msl/2d_util.cpp src/msl/font.cpp src/msl/glut_input.cpp src/msl/socket.cpp src/msl/socket_util.cpp src/msl/sprite.cpp src/msl/string_util.cpp src/msl/time_util.cpp
 set SOIL=windows\mingw\include\SOIL\SOIL.c windows\mingw\include\SOIL\stb_image_aug.c
 set SRC=src/example.cpp src/falconer.cpp %MSL% %SOIL%
-set OPT=-O -Wall -Wno-unknown-pragmas -static-libgcc -static-libstdc++
+set OPTS=-O -Wall -Wno-unknown-pragmas -static-libgcc -static-libstdc++
 set LIB=-lopengl32 -lglu32 -lfreeglut -lfreeglut_static -lglew32 -lwsock32 -lavcodec -lavdevice -lavfilter -lavformat -lavutil -lx264 -ltheoraenc -logg -ltheoradec -lswscale
 set COMPILER=windows\mingw\bin\g++.exe
 set OUT=bin
