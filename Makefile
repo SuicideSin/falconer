6bc9311926c1b937f1dbc5995efc9f75e112d2b5
Your Name <you@example.com>
2014-05-22 14:14:41 -0800

    Made this thing compile with the Makefile...moved msl and didn't change the Makefile!!! T_T

--- Makefile
+++ Makefile
@@ -1,14 +1,15 @@
 MSL=src/msl/2d.cpp src/msl/2d_util.cpp src/msl/glut_input.cpp src/msl/socket.cpp src/msl/socket_util.cpp src/msl/sprite.cpp src/msl/string_util.cpp src/msl/time_util.cpp
-SRC=src/example.cpp src/falconer.cpp $(MSL)
+SRC=src/example.cpp falconer.cpp $(MSL)
 OPTS=-O -Wall -I/usr/include/freetype2
 LIB=-lGL -lGLU -lGLEW -lglut -lSOIL -lavcodec -lswscale -lavutil -lftgl
 COMPILER=g++
 OUT=bin
+DIRS=-Isrc -I.
 
 all: falconer
 
 falconer: $(SRC)
-	$(COMPILER) $(CFLAGS) $(SRC) $(LIB) $(OPTS) -o $(OUT)/$@
+	$(COMPILER) $(CFLAGS) $(DIRS) $(SRC) $(LIB) $(OPTS) -o $(OUT)/$@
 
 clean:
 	rm -rf $(OUT)/falconer
--- make.bat
+++ make.bat
@@ -2,11 +2,12 @@
 
 set MSL=src/msl/2d.cpp src/msl/2d_util.cpp src/msl/font.cpp src/msl/glut_input.cpp src/msl/socket.cpp src/msl/socket_util.cpp src/msl/sprite.cpp src/msl/string_util.cpp src/msl/time_util.cpp
 set SOIL=windows\mingw\include\SOIL\SOIL.c windows\mingw\include\SOIL\stb_image_aug.c
-set SRC=src/example.cpp src/falconer.cpp %MSL% %SOIL%
+set SRC=src/example.cpp falconer.cpp %MSL% %SOIL%
 set OPTS=-O -Wall -Wno-unknown-pragmas -static-libgcc -static-libstdc++
 set LIB=-lopengl32 -lglu32 -lfreeglut -lfreeglut_static -lglew32 -lwsock32 -lavcodec -lavdevice -lavfilter -lavformat -lavutil -lx264 -ltheoraenc -logg -ltheoradec -lswscale
 set COMPILER=windows\mingw\bin\g++.exe
 set OUT=bin
+set DIRS=-Isrc -I.
 
 REM BUILD VARIABLES
 	SET BUILD_FALCONER=0
@@ -25,5 +26,5 @@ REM CHECK COMMAND LINE OPTIONS
 	IF %ARGC%==0 SET BUILD_FALCONER=1
 
 REM BUILD WHAT IS NEEDED
-	IF %BUILD_FALCONER%==1 %COMPILER% %SRC% %LIB% %OPTS% -o %OUT%/falconer.exe
+	IF %BUILD_FALCONER%==1 %COMPILER% %DIRS% %SRC% %LIB% %OPTS% -o %OUT%/falconer.exe
 	IF %BUILD_CLEAN%==1 DEL /Q %OUT%\falconer.exe
